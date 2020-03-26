6d9820edf6d2b5dae9366d69bd4e1e4ef579cf33
Mattias Plaisier <mattias.plaisier@flowpilots.com>
2020-03-26 18:04:24 +0100

    Add UserInterface to show alerts from VM

--- ThePage/src/ThePage.Droid/Setup.cs
+++ ThePage/src/ThePage.Droid/Setup.cs
@@ -1,9 +1,17 @@
-﻿using MvvmCross.Droid.Support.V7.AppCompat;
+﻿using MvvmCross;
+using MvvmCross.Droid.Support.V7.AppCompat;
 using ThePage.Core;
 
 namespace ThePage.Droid
 {
     public class Setup : MvxAppCompatSetup<App>
     {
+        protected override void InitializeFirstChance()
+        {
+            base.InitializeFirstChance();
+
+            Mvx.IoCProvider.RegisterType<IUserInteraction, UserInteraction>();
+        }
+
     }
 }
