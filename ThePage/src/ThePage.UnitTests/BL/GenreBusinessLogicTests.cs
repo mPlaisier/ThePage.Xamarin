9962fa3e12c467fb569d70721a68f1f9c3258698
Mattias Plaisier <mattias.plaisier@flowpilots.com>
2020-04-02 23:17:43 +0200

    add the old unit tests

--- ThePage/src/ThePage.UnitTests/BL/GenreBusinessLogicTests.cs
+++ ThePage/src/ThePage.UnitTests/BL/GenreBusinessLogicTests.cs
@@ -7,14 +7,49 @@ namespace ThePage.UnitTests
 {
     public class GenreBusinessLogicTests
     {
-        public GenreBusinessLogicTests()
+        [Fact]
+        public void CheckFilledListGenreConvertedToCellGenres()
         {
+            //Create List<Genre>
+            var genres = new List<Genre>
+            {
+                new Genre("1","Fiction"),
+                new Genre("2","Fantasy")
+            };
+
+            //Execute
+            var cellGenres = GenreBusinessLogic.GenresToCellGenres(genres);
+
+            //Check
+            Assert.NotNull(cellGenres);
+            Assert.NotEmpty(cellGenres);
         }
 
         [Fact]
-        public void TestFunction()
+        public void CheckEmptyListGenreConvertedToCellGenres()
         {
-            Assert.True(true);
+            //Create List<Genre>
+            var genres = new List<Genre>();
+
+            //Execute
+            var cellGenres = GenreBusinessLogic.GenresToCellGenres(genres);
+
+            //Assert
+            Assert.NotNull(cellGenres);
+            Assert.Empty(cellGenres);
+        }
+
+        [Fact]
+        public void CheckNullListGenreConvertedToCellGenres()
+        {
+            //Create List<Genre>
+            List<Genre> genres = null;
+
+            //Execute
+            var cellGenres = GenreBusinessLogic.GenresToCellGenres(genres);
+
+            //Assert
+            Assert.Null(cellGenres);
         }
     }
 }
