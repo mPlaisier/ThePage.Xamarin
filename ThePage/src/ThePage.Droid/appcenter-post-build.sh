0f3672a78b624042ee607185ea9eb610441170cd
Mattias Plaisier <mattias.plaisier@flowpilots.com>
2020-04-02 22:07:27 +0200

    remove not required logging

--- ThePage/src/ThePage.Droid/appcenter-post-build.sh
+++ ThePage/src/ThePage.Droid/appcenter-post-build.sh
@@ -12,17 +12,12 @@ echo "XUnit tests result:"
 pathOfTestResults=$(find "$APPCENTER_SOURCE_DIRECTORY"/.*/ThePage/src/*Tests/TestResults/* -name 'testresult.trx')
 
 grep ' \[FAIL\]' $pathOfTestResults
-echo "Done grep"
 failures=$(grep -o ' \[FAIL\]' $pathOfTestResults | wc -l)
 
 if [[ $failures -eq 0 ]]
 then 
-echo 
 echo "Unit Tests passed" 
-echo
 else 
-echo
 echo "Unit Tests failed!"
-echo
 exit 1
 fi
