3c467ffa4aed810df6d9bea84625caa66da59bd9
Mattias Plaisier <mattias.plaisier@flowpilots.com>
2020-06-17 23:46:25 +0200

    #95 add Droid fragment with layout file

---
+++ ThePage/src/ThePage.Droid/Views/Authenticatioin/LoginViewFragment.cs
@@ -0,0 +1,52 @@
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+using ThePage.Core;
+using ThePage.Core.ViewModels.Main;
+using ThePage.Droid.Views;
+
+namespace ThePage.Droid
+{
+    [MvxFragmentPresentation(typeof(MainContainerViewModel),
+       Resource.Id.content_frame,
+       AddToBackStack = true,
+       EnterAnimation = Resource.Animation.pull_in_down,
+       ExitAnimation = Resource.Animation.push_out_up,
+       PopEnterAnimation = Resource.Animation.pull_in_up,
+       PopExitAnimation = Resource.Animation.push_out_down
+   )]
+    public class LoginViewFragment : BaseFragment<LoginViewModel>, ViewTreeObserver.IOnGlobalFocusChangeListener
+    {
+        #region Properties
+
+        protected override int FragmentLayoutId => Resource.Layout.fragment_login;
+
+        #endregion
+
+        #region LifeCycle
+
+        public override void OnResume()
+        {
+            base.OnResume();
+            View.ViewTreeObserver.AddOnGlobalFocusChangeListener(this);
+        }
+
+        public override void OnPause()
+        {
+            base.OnPause();
+            View.ViewTreeObserver.RemoveOnGlobalFocusChangeListener(this);
+        }
+
+        #endregion
+
+        #region IOnGlobalFocusChangeListener
+
+        public void OnGlobalFocusChanged(View oldFocus, View newFocus)
+        {
+            if (!(newFocus is EditText))
+                DroidUtils.HideKeyboard(Activity);
+        }
+
+        #endregion
+    }
+}
--- ThePage/src/ThePage.Droid/Views/Main/MainFragment.cs
+++ ThePage/src/ThePage.Droid/Views/Main/MainFragment.cs
@@ -14,7 +14,14 @@ using MvvmCross.Platforms.Android.Presenters.Attributes;
 
 namespace ThePage.Droid.Views.Main
 {
-    [MvxFragmentPresentation(typeof(MainContainerViewModel), Resource.Id.content_frame)]
+    [MvxFragmentPresentation(typeof(MainContainerViewModel),
+       Resource.Id.content_frame,
+       AddToBackStack = true,
+       EnterAnimation = Resource.Animation.pull_in_down,
+       ExitAnimation = Resource.Animation.push_out_up,
+       PopEnterAnimation = Resource.Animation.pull_in_up,
+       PopExitAnimation = Resource.Animation.push_out_down
+   )]
     public class MainFragment : BaseFragment<MainViewModel>
     {
         protected override int FragmentLayoutId => Resource.Layout.fragment_main;
