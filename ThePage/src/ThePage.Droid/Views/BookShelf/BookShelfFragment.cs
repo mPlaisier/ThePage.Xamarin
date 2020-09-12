f689eb81dc30e3551212ce725255dfd0996f8bed
Mattias Plaisier <mattias.plaisier@flowpilots.com>
2020-09-12 23:24:34 +0200

    #125 Setup Droid project to show BookShelves overview, add layout files

---
+++ ThePage/src/ThePage.Droid/Views/BookShelf/BookShelfFragment.cs
@@ -0,0 +1,24 @@
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+using ThePage.Core.ViewModels.Main;
+using ThePage.Droid.Views;
+
+namespace ThePage.Droid
+{
+    [MvxFragmentPresentation(typeof(MainContainerViewModel),
+        Resource.Id.content_frame,
+        AddToBackStack = true,
+        EnterAnimation = Resource.Animation.pull_in_right,
+        ExitAnimation = Resource.Animation.push_out_left,
+        PopEnterAnimation = Resource.Animation.pull_in_left,
+        PopExitAnimation = Resource.Animation.push_out_right
+    )]
+    public class BookShelfFragment : BaseFragment<BookShelfViewModel>
+    {
+        #region Properties
+
+        protected override int FragmentLayoutId => Resource.Layout.fragment_bookshelf;
+
+        #endregion
+
+    }
+}
