9243673e60e24278b08a7e7cd4456172730b493c
Mattias Plaisier <mattias.plaisier@flowpilots.com>
2020-09-26 23:44:22 +0200

    #121 Add unit tests for AddBookShelfVM

---
+++ ThePage/src/ThePage.UnitTests/ViewModels/BookShelf/AddBookShelfViewModelTests.cs
@@ -0,0 +1,99 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using ThePage.Core;
+using Xunit;
+using static ThePage.Core.CellBookInput;
+
+namespace ThePage.UnitTests.ViewModels.BookShelf
+{
+    public class AddBookShelfViewModelTests : BaseViewModelTests
+    {
+        readonly AddBookShelfViewModel _vm;
+
+        #region Constructor
+
+        public AddBookShelfViewModelTests()
+        {
+            Setup();
+
+            _vm = Ioc.IoCConstruct<AddBookShelfViewModel>();
+        }
+
+        #endregion
+
+        #region Setup
+
+        void LoadViewModel()
+        {
+            _vm.Initialize();
+        }
+
+        #endregion
+
+        [Fact]
+        public void CheckItemCountAtStart()
+        {
+            //Setup
+            LoadViewModel();
+
+            //Assert
+            _vm.Items.Should().HaveCount(4);
+        }
+
+        [Fact]
+        public void BtnDisabledAtStart()
+        {
+            //Setup
+            LoadViewModel();
+
+            //Assert
+            _vm.IsValid.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ViewModelIsNotLoadingAtStartup()
+        {
+            //Setup
+            LoadViewModel();
+
+            //Assert
+            _vm.IsLoading.Should().BeFalse();
+        }
+
+        [Fact]
+        public void CheckIfTypesAreCorrectOfCellItemsAtStartup()
+        {
+            //Setup
+            LoadViewModel();
+
+            //Assert
+            _vm.Items[0].Should().BeOfType<BaseCellTitle>();
+            _vm.Items[1].Should().BeOfType<CellBookShelfTextView>();
+            _vm.Items[2].Should().BeOfType<BaseCellTitle>();
+            _vm.Items[3].Should().BeOfType<BaseCellClickableText>();
+        }
+
+        [Theory]
+        [InlineData("a", true)]
+        [InlineData("valid", true)]
+        [InlineData("abcdefghijklmopqrstuvwxyz123456789", true)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        public void BtnIsEnabledWithValidInput(string name, bool isValid)
+        {
+            //Setup
+            LoadViewModel();
+
+            //Execute
+            var cellName = _vm.Items.OfType<CellBookShelfTextView>().Where(x => x.InputType == EBookShelfInputType.Name).First();
+            cellName.TxtInput = name;
+
+            //Assert
+            _vm.IsValid.Should().Be(isValid);
+
+        }
+    }
+}
