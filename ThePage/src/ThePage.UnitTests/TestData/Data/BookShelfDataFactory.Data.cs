8d337899afd49849ed6414592d1701038492232f
Mattias Plaisier <mattias.plaisier@flowpilots.com>
2020-10-10 12:24:45 +0200

    #119 add Unit tests

--- ThePage/src/ThePage.UnitTests/TestData/Data/BookShelfDataFactory.Data.cs
+++ ThePage/src/ThePage.UnitTests/TestData/Data/BookShelfDataFactory.Data.cs
@@ -44,5 +44,62 @@ namespace ThePage.UnitTests
                 ""prevPage"": null,
                 ""nextPage"": null
             }";
+
+        const string BookShelfDetailResponseWithBooks =
+            @"{
+                ""books"": [
+                {
+                    ""title"": ""title 1"",
+                    ""author"": {
+                    ""name"": ""name 1"",
+                    ""id"": ""5f369567ab0b90001703d5df""
+                },
+                    ""id"": ""5f7065b62e98b70017ac3ed1""
+                },
+                {
+                    ""title"": ""titel 2"",
+                    ""author"": {
+                        ""name"": ""author 1"",
+                        ""id"": ""5eda16f40e0f1e6d7765e824""
+                    },
+                    ""id"": ""5f56950327b2c30017cbd2a5""
+                },
+                {
+                    ""title"": ""title 3"",
+                    ""author"": {
+                        ""name"": ""author 2"",
+                        ""id"": ""5ebe620b77a70d9bb9fb5c20""
+                    },
+                    ""id"": ""5eda29b141a1ab74802ba736""
+                }
+                ],
+                ""name"": ""Bookshelf name"",
+                ""id"": ""5f7066162e98b70017ac3ed3""
+            }";
+
+        const string BookShelfDetailResponseWithoutBooks =
+            @"{
+                ""books"": [],
+                ""name"": ""Bookshelf name"",
+                ""id"": ""5f7066162e98b70017ac3ed3""
+            }";
+
+        const string SingleBookfShelfWithBooks =
+            @"{
+                ""books"": [
+                    ""5eda29b141a1ab74802ba736"",
+                    ""5f56950327b2c30017cbd2a5"",
+                    ""5f7065b62e98b70017ac3ed1""
+                ],
+                ""name"": ""Bookshelf name"",
+                ""id"": ""5f7066162e98b70017ac3ed3""
+            }";
+
+        const string SingleBookfShelfWithoutBooks =
+             @"{
+                ""books"": [],
+                ""name"": ""Bookshelf name"",
+                ""id"": ""5f7066162e98b70017ac3ed3""
+            }";
     }
 }
