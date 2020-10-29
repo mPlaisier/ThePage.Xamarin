028b6cae6dd28425fdd56b136e03e89be52ac1cf
Mattias Plaisier <mattias.plaisier@flowpilots.com>
2020-10-29 20:32:21 +0100

    #147 code cleanup

--- ThePage/src/ThePage.Core/Services/AuthService.cs
+++ ThePage/src/ThePage.Core/Services/AuthService.cs
@@ -3,7 +3,6 @@ using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AppCenter.Crashes;
 using MonkeyCache.LiteDB;
-using MvvmCross.Navigation;
 using Newtonsoft.Json;
 using Refit;
 using ThePage.Api;
