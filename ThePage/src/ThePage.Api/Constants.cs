a68b07fc7994e7007b06e6159a14ddc65e73e8db
Mattias Plaisier <mattias.plaisier@flowpilots.com>
2020-12-30 15:08:21 +0100

    fix build error release

--- ThePage/src/ThePage.Api/Constants.cs
+++ ThePage/src/ThePage.Api/Constants.cs
@@ -1,7 +1,7 @@
 using System;
 namespace ThePage.Api
 {
-    public class Constants
+    public static class Constants
     {
         public const int GenreExpirationTimeInMinutes = 60;
         public const int AuthorExpirationTimeInMinutes = 60;
@@ -10,7 +10,7 @@ namespace ThePage.Api
 #if DEBUG
         public const string ThePage_Api_Url = "https://thepageapi-stg.herokuapp.com/api";
 #else
-        public string ThePage_Api_Url = "https://thepageapi.herokuapp.com/api";
+        public const string ThePage_Api_Url = "https://thepageapi.herokuapp.com/api";
 #endif
 
         public const string OpenLibrary_Api_Url = "http://openlibrary.org/api";
